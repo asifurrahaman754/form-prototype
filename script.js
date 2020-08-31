f1bd7791bd71357f54e0a70347b8d4a5a27bf26e
iamasifur <asifurrahaman754@gmail.com>
2020-08-31 21:28:35 +0600

    new files added

---
+++ script.js
@@ -0,0 +1,14 @@
+    
+        let left = document.querySelector('.left');
+        let content = document.querySelector('.leftContent');
+        let protos = document.querySelectorAll('.proto');
+
+        left.addEventListener('mousemove', (event) => {
+            let move1 = (event.clientX * 0.05);
+            let move2 = (event.clientX * 0.003);
+            content.style.transform = `translateX(-${move2}%)`;
+
+            protos.forEach((proto) => {
+                proto.style.transform = `translateX(${move1}%)`;
+            })
+        })
